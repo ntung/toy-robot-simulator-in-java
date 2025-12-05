104bf971cce192cb014ee602e2a2cc4c525e735d
Tung Nguyen <nvntung@gmail.com>
2025-12-05 19:55:07 +0000

    Update the programme instructions

--- src/main/java/com/cellularorigins/Main.java
+++ src/main/java/com/cellularorigins/Main.java
@@ -1,5 +1,7 @@
 package com.cellularorigins;
 
+import com.cellularorigins.production.ToyRobotFactory;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
@@ -11,7 +13,9 @@ public class Main {
             if (args.length != 1 || args[0].isEmpty()) {
                 System.out.println("Usage: java -jar ToyRobotSimulatorV1.0-SNAPSHOT-jar-with-dependencies.jar <file of commands>");
             } else {
-                System.out.println("Play game!");
+                System.out.println("Play robot game now! Commands: MOVE X,Y,[NORTH, EAST, SOUTH, WEST]; LEFT; RIGHT; " +
+                        "REPORT. Type quit to exit the game.");
+                ToyRobotFactory.createToyRobot();
             }
         } catch (Exception e) {
             System.out.println("Error: " + e.getMessage());
