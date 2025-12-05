c082262aec0ab1288c8c3d869c8cc2f37f664d12
Tung Nguyen <nvntung@gmail.com>
2025-12-05 09:42:26 +0000

    Update Java docstrings

--- src/main/java/com/cellularorigins/core/Command.java
+++ src/main/java/com/cellularorigins/core/Command.java
@@ -1,5 +1,10 @@
 package com.cellularorigins.core;
 
+/**
+ * An enum class representing the commands of the robot.
+ * As of writing this code, players can execute the following commands: <strong>PLACE, MOVE, LEFT, RIGHT</strong> and
+ * <strong>REPORT</strong>.
+ */
 public enum Command {
     PLACE,
     MOVE,
--- src/main/java/com/cellularorigins/core/Direction.java
+++ src/main/java/com/cellularorigins/core/Direction.java
@@ -1,5 +1,8 @@
 package com.cellularorigins.core;
 
+/**
+ * An enum class representing four universal directions which a robot can move.
+ */
 public enum Direction {
     NORTH("NORTH"),
     EAST("EAST"),
@@ -8,10 +11,22 @@ public enum Direction {
 
     private String description;
 
+    /**
+     * Default constructor
+     * @param description A {@link String} object denoting the direction,
+     *                    for example, "north" or "NORTH"
+     */
     Direction(String description) {
         this.description = description;
     }
 
+    /**
+     * This static method returns a direction based on the string representing it.
+     * @param description A {@link String} object denoting the direction,
+     *                    for example, "north" or "NORTH"
+     * @return a {@link Direction} if the description is one of the four universal directions. Otherwise, it returns
+     * null.
+     */
     public static Direction from(String description) {
         for (Direction direction : values()) {
             if (direction.getDescription().equalsIgnoreCase(description)) {
@@ -21,11 +36,20 @@ public enum Direction {
         return null;
     }
 
+    /**
+     * This method returns a direction after executing a command.
+     * @param rotatePoint it can be either 1 or -1
+     * @return a {@link Direction} one of the four universal directions
+     */
     public Direction changeDirection(int rotatePoint) {
         int resultIndex = ((ordinal() + rotatePoint) + 4) % 4;
         return values()[resultIndex];
     }
 
+    /**
+     * This method returns the description of an individual {@link Direction} instance.
+     * @return A {@link String} representing the name of the given direction.
+     */
     public String getDescription() {
         return description;
     }
