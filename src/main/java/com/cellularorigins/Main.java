7c14d8ec6a11fcdd978863d320a4e25128a45912
Tung Nguyen <nvntung@gmail.com>
2025-12-05 15:02:23 +0000

    Build a fat JAR file with all dependencies

--- src/main/java/com/cellularorigins/Main.java
+++ src/main/java/com/cellularorigins/Main.java
@@ -6,6 +6,15 @@ public class Main {
     public static void main(String[] args) {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Welcome to Cellular Origins!\n!");
+        System.out.print("Welcome to Cellular Origins!\n");
+        try  {
+            if (args.length != 1 || args[0].isEmpty()) {
+                System.out.println("Usage: java -jar ToyRobotSimulatorV1.0-SNAPSHOT-jar-with-dependencies.jar <file of commands>");
+            } else {
+                System.out.println("Play game!");
+            }
+        } catch (Exception e) {
+            System.out.println("Error: " + e.getMessage());
+        }
     }
 }
