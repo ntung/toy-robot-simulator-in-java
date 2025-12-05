8376acfa342b3c6af6eff82e59f1bf9a7f755f63
Tung Nguyen <nvntung@gmail.com>
2025-12-05 20:42:06 +0000

    Build a factory class to create Toy Robot

---
+++ src/main/java/com/cellularorigins/production/ToyRobotFactory.java
@@ -0,0 +1,80 @@
+package com.cellularorigins.production;
+
+import com.cellularorigins.core.Command;
+import com.cellularorigins.core.Direction;
+import com.cellularorigins.core.Position;
+import com.cellularorigins.exception.IllegalActionException;
+import com.cellularorigins.exception.InvalidRobotException;
+import com.cellularorigins.simulation.GameBoard;
+import com.cellularorigins.simulation.Robot;
+import com.cellularorigins.simulation.Simulator;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.Scanner;
+
+@Getter @Setter
+public class ToyRobotFactory {
+
+    public static void createToyRobot() {
+        GameBoard gameBoard = new GameBoard(5, 5);
+        Position startPosition = new Position(0, 0, Direction.NORTH);
+        Robot robot = new Robot(startPosition);
+        Simulator simulator = new Simulator(gameBoard, robot);
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            // receive input
+            String commandLine = scanner.nextLine();
+            if (commandLine.equalsIgnoreCase("quit")) {
+                break;
+            }
+            // parse the input and execute the command
+            String report = executeCommand(simulator, commandLine);
+            System.out.println(report);
+        }
+    }
+
+    public static String executeCommand(Simulator simulator, String commandLine) {
+        String[] commandArgs = commandLine.split(" ");
+        String output = "";
+        try {
+            Command command = Command.from(commandArgs[0]);
+            if (command == Command.PLACE) {
+                try {
+                    if (commandArgs.length == 1) {
+                        return "Invalid command";
+                    }
+                    boolean result = placeCommand(simulator, commandArgs[1]);
+                    System.out.println("Place command " + command.getDescription() + " " + (result ? "successfully" :
+                            "unsuccessfully"));
+                }  catch (IllegalActionException e) {
+                    throw new IllegalActionException(e.getMessage());
+                }
+            } else {
+                try {
+                    output = simulator.play(command);
+                } catch (InvalidRobotException | IllegalActionException exception) {
+                    System.out.println(exception.getMessage() + ". Try again with the built-in commands!");
+                }
+            }
+        } catch (IllegalArgumentException ex) {
+            throw new InvalidRobotException("Invalid command");
+        }
+        return output;
+    }
+
+    public static boolean placeCommand(Simulator simulator, String commands) {
+        int xValue;
+        int yValue;
+        String[] commandArgs = commands.split(",");
+        try {
+            xValue = Integer.parseInt(commandArgs[0]);
+            yValue = Integer.parseInt(commandArgs[1]);
+        } catch (Exception ex) {
+            throw new InvalidRobotException("Invalid argument");
+        }
+        Direction direction = Direction.from(commandArgs[2]);
+        Position position = new Position(xValue, yValue, direction);
+        return simulator.placeRobot(position);
+    }
+}
---
+++ src/test/java/com/cellularorigins/production/ToyRobotFactoryTest.java
@@ -0,0 +1,58 @@
+package com.cellularorigins.production;
+
+import com.cellularorigins.core.Command;
+import com.cellularorigins.core.Direction;
+import com.cellularorigins.core.Position;
+import com.cellularorigins.exception.InvalidRobotException;
+import com.cellularorigins.simulation.GameBoard;
+import com.cellularorigins.simulation.Robot;
+import com.cellularorigins.simulation.Simulator;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+public class ToyRobotFactoryTest {
+    private final Simulator simulator = new Simulator();
+    @BeforeEach
+    public void setUp() {
+        GameBoard gameboard = new GameBoard(5, 5);
+        Position startPosition = new Position(0, 0, Direction.NORTH);
+        Robot robot = new Robot(startPosition);
+        simulator.setRobot(robot);
+        simulator.setGameboard(gameboard);
+    }
+
+    @Test
+    public void testExecuteCommand() {
+        String report = ToyRobotFactory.executeCommand(simulator, "REPORT");
+        Assertions.assertNotNull(report);
+        Assertions.assertEquals("0, 0, NORTH", report);
+    }
+
+    @Test
+    public void testExecuteInvalidCommand() {
+        String result = ToyRobotFactory.executeCommand(simulator, "PLACE");
+        Assertions.assertEquals("Invalid command", result);
+    }
+
+    @Test
+    public void testPlaceCommand() {
+        boolean actual = ToyRobotFactory.placeCommand(simulator, "0,0,NORTH");
+        Assertions.assertTrue(actual);
+        // this place command tries to put the robot outside the board
+        actual = ToyRobotFactory.placeCommand(simulator, "0,-1,NORTH");
+        Assertions.assertFalse(actual);
+    }
+
+    @Test
+    public void testPlaceInvalidCommand() {
+        InvalidRobotException thrown = assertThrows(
+            InvalidRobotException.class,
+            () -> ToyRobotFactory.placeCommand(simulator, "PLACE 0,0,NORTH"),
+            "Expected play() to throw an exception when the command is invalid"
+        );
+        Assertions.assertFalse(thrown.getMessage().contains("Invalid command"));
+    }
+}
